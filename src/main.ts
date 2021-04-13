0907ed363a946e5c6b1877feacb6e6e7c75b6b50
Yoni <firroloni.yoni@gmail.com>
2021-04-13 11:12:57 +0200

    set type for trust proxy

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap(){
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
     credentials: true,
